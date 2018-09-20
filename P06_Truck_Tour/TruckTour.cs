442970378e27a842e832379b8b458df9ccb22423
Svetlin Stoyanov <svetlinst@gmail.com>
2018-09-21 00:43:33 +0300

    1

---
+++ P06_Truck_Tour/TruckTour.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace P06_Truck_Tour
+{
+    class TruckTour
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPumps = int.Parse(Console.ReadLine());
+            Queue<long[]> pumpInfo = new Queue<long[]>(numberOfPumps);
+
+            for (int i = 0; i < numberOfPumps; i++)
+            {
+                long[] curPump = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+                pumpInfo.Enqueue(curPump);
+            }
+
+            int index = 0;
+            while (true)
+            {
+                long totalFuel = 0;
+                foreach (var pump in pumpInfo)
+                {
+                    var fuel = pump[0];
+                    var distance = pump[1];
+                    totalFuel += fuel - distance;
+
+                    if (totalFuel < 0)
+                    {
+                        index++;
+                        pumpInfo.Enqueue(pumpInfo.Dequeue());
+                        break;
+                    }
+                }
+
+                if (totalFuel >= 0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(index);
+        }
+    }
+}
---
+++ P07_Balanced_Parentheses/BalancedParentheses.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07_Balanced_Parentheses
+{
+    class BalancedParentheses
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Stack<char> parentheses = new Stack<char>(input.Length);
+            char[] openingBrackets = new char[]{'(', '[', '{'};
+            bool correctInput = true;
+            bool isBalanced = true;
+            foreach (var ch in input)
+            {
+                if (openingBrackets.Contains(ch))
+                {
+                    parentheses.Push(ch);
+                    continue;
+                }
+
+                if (parentheses.Count!=0)
+                {
+                    switch (ch)
+                    {
+                        case ')':
+                            if (parentheses.Peek() == '(')
+                            {
+                                parentheses.Pop();
+                            }
+                            break;
+                        case ']':
+                            if (parentheses.Peek() == '[')
+                            {
+                                parentheses.Pop();
+                            }
+                            break;
+                        case '}':
+                            if (parentheses.Peek() == '{')
+                            {
+                                parentheses.Pop();
+                            }
+                            break;
+                    }
+                }
+                else
+                {
+                    correctInput = false;
+                }
+   
+            }
+
+            if (parentheses.Count==0 && correctInput)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
